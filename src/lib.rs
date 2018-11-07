5eae08f73c4a349e1708203bc10a513879f854d1
Demi Obenour <demiobenour@gmail.com>
2018-11-07 15:20:16 -0500

    Initial commit of winpath.rs
    Mostly works (with tests!), though not everything is implemented.

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![config_attr(not(windows), forbid(unsafe_code))]
+mod dotdot;
+pub use dotdot::*;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
